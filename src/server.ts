74f44ae81360100835713a9017f97212274a8158
Wendell Machado <wendellmachado.dev@gmail.com>
2021-04-10 13:38:10 -0300

    changing port env name

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
